1d1b0ee0eb6dbe003a8248541733c4e6922d2978
PeterParkinsonss <160024324+PeterParkinsonss@users.noreply.github.com>
2024-12-10 19:36:06 -0800

    2nd test halfway done

--- app/src/androidTest/java/com/example/login/ClearDataBaseTest.java
+++ app/src/androidTest/java/com/example/login/ClearDataBaseTest.java
@@ -1,4 +1,34 @@
 package com.example.login;
 
+
+import android.content.Context;
+
+import androidx.room.Room;
+import androidx.test.core.app.ApplicationProvider;
+
+import org.junit.After;
+import org.junit.Before;
+
+
 public class ClearDataBaseTest {
+    private AppDatabase database;
+    private UserDao userDao;
+    private SudokuPuzzleDao puzzleDao;
+
+    @Before
+    public void setUp() {
+        Context context = ApplicationProvider.getApplicationContext();
+        database = Room.inMemoryDatabaseBuilder(context, AppDatabase.class)
+                .allowMainThreadQueries()
+                .build();
+        userDao = database.userDao();
+        puzzleDao = database.sudokuPuzzleDao();
+    }
+
+    @After
+    public void tearDown() {
+        database.close();
+    }
+
+
 }
